10ae8961c1f861db4090c7d7b0480c9e2dccf74d
luochuangjie <luochuangjie@lightinthebox.com>
2013-08-21 19:09:22 +0800

    change files and their path

--- template/index/base.js
+++ template/index/base.js
@@ -1,18 +1,15 @@
 !function ($) {
+	var template = "";
+	var categorySelected = "";
 	var addArticle = function(data){
-		var template = '<div class="span4" cateory="'+data.articleCategory+'"><h2>'+data.articleTitle+
-		'</h2><div class="author">author: <em>'+data.articleAuthor+
-		'</em></div><p>'+data.article+'</p><p><a class="btn" href="#">View details &raquo;</a></p></div>';
-		$("#article").append(template);
+		template = template + '<div class="span3" category="'+data.articleCategory+'"><div class="tile">'+
+                    '<img src="../lib/images/1.jpg" alt="picture" class="tile-image"><h3 class="tile-title">'+data.articleTitle+
+                    '</h3><span class="info">Author:<span id="author">'+data.articleAuthor+
+                    '</span></span><p>'+data.article+
+                    '</p><a class="btn btn-primary btn-large btn-block" href="../article/article.html?article='+data.articleId+
+                    '">View More &raquo;</a></div></div>';
 	}
-	var composing = function(){
-		$("#article .span4").each(function(index){
-			if(index%3 === 0){
-				$(this).css("margin-left",0);
-			}
-		});
-	}
-	window.onload = function(){
+	$(function(){
 		var phpUrl = "../php/readme.json";
 		$.ajax({
 			type	 : "GET",
@@ -22,8 +19,62 @@
 				for(var i = 0; i < data.articlesList.length ; i++){
 					addArticle(data.articlesList[i]);
 				}
-				composing();
+				$(".span12").html(template);
 			}
 		});
-	}
+	});
+
+	$(".todo li").click(function(e) {
+		e.preventDefault();
+		$(".todo li.todo-done").removeClass("todo-done");
+		if(categorySelected != $(this).attr("id")){
+			$(this).addClass("todo-done");
+			categorySelected = $(this).attr("id");
+		}else{
+			categorySelected = "";
+		}
+    });
+	$(".btn-primary").click(function(e){
+		e.preventDefault();
+		var testUrl = "../php/readme2.json";
+		var author = $("#author").val();
+		var article = $("#article").val();
+		if(author === ""){
+			author = 0;
+		}
+		if(article === ""){
+			article = 0;
+		}
+		switch(categorySelected)
+		{
+			case "categoryOne":
+		  		categorySelected = 1;
+		  		break;
+			case "categoryTwo":
+		 		 categorySelected = 2;
+		  		break;
+		 	case "categoryThree":
+		  		categorySelected = 3;
+		  		break;
+		  	case "categoryFour":
+		  		categorySelected = 4;
+		  		break;
+			default:
+				categorySelected = 0;
+		}
+		$.ajax({
+			type	 : "GET",
+			url		 : testUrl,
+			dataType : "json",
+			data     : { categorySelected: categorySelected, author: author, article: article },
+			success  : function(data){
+				template = "";
+				for(var i = 0; i < data.articlesList.length ; i++){
+					addArticle(data.articlesList[i]);
+				}
+				$(".span12").html(template);
+			}
+		});
+	});
+
 }(window.jQuery);
