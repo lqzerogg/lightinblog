b17edff0b633961d862e54c12f4444cc7cc50244
zhangzhanhe1114 <zhanhe1114@163.com>
2013-08-22 20:16:44 +0800

    php

--- template/php/blog.php
+++ template/php/blog.php
@@ -1,5 +1,30 @@
 <?php
-	$address=$_GET['address'];
-	$file=fopen($address,"r") or exit("Unable to open file!");
-	echo json_encode($file);
+    mysql_query("SET NAMES 'UTF8'"); 
+	$db=new mysqli("localhost","root","","lightinblog");
+    if(mysqli_connect_errno())
+	{
+			echo "error";
+	}
+    $data = [];
+    $id = 1;
+	$sql="select 
+		 a.title,
+		 a.author,
+		 a.category,
+		 a.address,
+		 a.introduction
+		from article a
+		where a.id is not null and a.id in (".$id.") ";				
+	$result=$db->query($sql);
+	$num_results=$result->num_rows;
+	$row=$result->fetch_assoc();
+	$contents = file_get_contents($row['address']);
+	$out=array(
+		'title'=>$row['title'],
+		'author'=>$row['author'],
+		'category'=>$row['category'],
+		'contents'=>$contents
+		);
+    array_push($data,$out);
+	echo json_encode($data);
 ?>
