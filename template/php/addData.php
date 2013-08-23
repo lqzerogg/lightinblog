3675ce8b745c4d9c1f5cd7266426bc9b44d35283
zhangzhanhe1114 <zhanhe1114@163.com>
2013-08-23 15:08:15 +0800

    addsomething

--- template/php/addData.php
+++ template/php/addData.php
@@ -1,22 +1,59 @@
 <?php
-	// mysql_query("SET NAMES 'UTF8'"); 
-	// $db=new mysqli("localhost","root","","lightinblog");
- //    if(mysqli_connect_errno())
-	// {
-	// 	echo "error";
+	// function mkdirs($dir, $mode = 0777)  
+	// {  
+	// 	if (is_dir($dir) || @mkdir($dir, $mode)){
+	// 		return TRUE;
+	// 	}  
+	// 	if (!mkdirs(dirname($dir), $mode)){
+	// 		return FALSE;
+	// 	}  
+	// 	return @mkdir($dir, $mode);  
 	// }
-	function mkdirs($dir, $mode = 0777)  
-	{  
-		if (is_dir($dir) || @mkdir($dir, $mode)){
-			return TRUE;
-		}  
-		if (!mkdirs(dirname($dir), $mode)){
-			return FALSE;
-		}  
-		return @mkdir($dir, $mode);  
+	function checkTableList($database,$table){
+		$result = mysql_list_tables($database);
+		$i = 0;        
+	    while($i<mysql_num_rows($result))
+	    {
+		    if ($table == mysql_tablename($result,$i)) {
+		        return true;
+		    }                          
+		    $i++;     
+		}
+			return false;
 	}
-	$path = "../../article";
-	$
+	mysql_query("SET NAMES 'UTF8'"); 
+	$con = mysql_connect("localhost","root","");
+    if (!$con)
+	{
+	  die('Could not connect: ' . mysql_error());
+	}
+	if(!mysql_select_db("lightinblog", $con)){
+		if (mysql_query("CREATE DATABASE lightinblog",$con))
+		{
+		  echo "Database created";
+		}
+		else
+		{
+		  echo "Error creating database: " . mysql_error();
+		}
+		mysql_select_db("lightinblog", $con);
+	}
+	if(!checkTableList("lightinblog","article")){
+		$table = "CREATE TABLE article
+			(
+				id int(15) primary key,
+				author char(30),
+				title char(50),
+				category int(5),
+				address char(30),
+				introduction char(200)
+			)";
+		if(mysql_query($table,$con)){
+			echo "creat table success";
+		}
+	}
+	$path = "../../articles/";
+	$mode = 0777;
 	$string = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	$introduction1 = "ANY one who loves simplicity or respects sincerity, any one who feels the tie binding us all together in the helplessness of our common human life";
 	$introduction2 = "This is a reproduction of a book published before 1923. This book may have occasional imperfections such as missing or blurred pages";
@@ -26,13 +63,26 @@
 	$titleArray = ["one world","two world","three world"];
 	$introductionArray = [$introduction1,$introduction2,$introduction3];
 	for($i = 0;$i < 100 ; $i++){
+		$file = "";
+		$contents = "";
 		$category = $categoryArray[rand(0,3)];
 		$author = $authorArray[rand(0,8)];
-		$title = $titleArray[rand(0,2)];
+		$title = $titleArray[rand(0,2)].rand(0,1000);
 		$introduction = $introductionArray[rand(0,2)];
-		for($j=0;$j<15;$j++){
-
+		for($j=0;$j<10;$j++){
+			$file = $file.$string[rand(0,25)];
 		}
-
+		for($k=0;$k<200;$k++){
+			if($k%6 == 0){
+				$contents = $contents." ";
+			}
+			$contents = $contents.$string[rand(0,25)];
+		}
+		$filePath = $path.$file.".txt";
+		$article = fopen($filePath,"w");
+		fwrite($article,$contents);
+		fclose($article);
+		$sql = "insert into article(id,author,title,category,address,introduction) values('$i','$author','$title','$category','$filePath','$introduction')";
+		$result=mysql_query($sql,$con);
 	}
 ?>
